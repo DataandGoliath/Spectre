8cca5306fbef527213e0f3a150616f239dd3ffdc
Name Redacted <29870550+DataandGoliath@users.noreply.github.com>
2018-05-04 21:52:23 +0000

    Update keygen.py

--- keygen.py
+++ keygen.py
@@ -2,6 +2,7 @@ import random
 import math
 import string
 from Crypto.PublicKey import RSA
+from Crypto.Hash import SHA256
 def isPrime(n):
     for i in range(2,int(n**0.5)+1):
         if n%i==0:
@@ -16,3 +17,7 @@ privatekey = key.exportKey(passphrase=code, pkcs=8)
 publickey = key.publickey().exportKey()
 print("PUBLIC KEY:\n"+str(publickey))
 print("\n\nPRIVATE KEY:\n"+str(privatekey))
+h=SHA256.new()
+h.update(publickey)
+print("\n\nPUBLIC KEY SHA256 HASH:\n"+h.hexdigest())
+#Only 22 lines? Wow. "Encryption is easy!" --No one, ever
