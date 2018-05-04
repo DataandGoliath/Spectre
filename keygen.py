a3e3a23d7e981637570559915875799f3195a27f
Name Redacted <29870550+DataandGoliath@users.noreply.github.com>
2018-05-04 21:48:12 +0000

    Add files via upload

---
+++ keygen.py
@@ -0,0 +1,18 @@
+import random
+import math
+import string
+from Crypto.PublicKey import RSA
+def isPrime(n):
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return False
+
+    return True
+N=random.randint(1000,99999)
+code=''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits+string.ascii_lowercase) for _ in range(N))
+#code = 'nooneknows'
+key = RSA.generate(4096)
+privatekey = key.exportKey(passphrase=code, pkcs=8)
+publickey = key.publickey().exportKey()
+print("PUBLIC KEY:\n"+str(publickey))
+print("\n\nPRIVATE KEY:\n"+str(privatekey))
